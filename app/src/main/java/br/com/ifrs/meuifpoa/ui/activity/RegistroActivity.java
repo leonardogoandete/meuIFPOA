60597c9256859c3e436c8dc388772259a9a6e73b
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-09-08 00:57:55 -0300

    Adicionado validação de CPF

--- app/src/main/java/br/com/ifrs/meuifpoa/ui/activity/RegistroActivity.java
+++ app/src/main/java/br/com/ifrs/meuifpoa/ui/activity/RegistroActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
 import br.com.ifrs.meuifpoa.R;
 import br.com.ifrs.meuifpoa.model.Registro;
 
@@ -25,11 +26,10 @@ public class RegistroActivity extends AppCompatActivity {
     private EditText etCpf;
     private EditText etEmail;
     private EditText etSenha;
-    private ProgressBar progressBar; // Declaração da ProgressBar
+    private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-    private CPFValidator cpfValidator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,66 +42,62 @@ public class RegistroActivity extends AppCompatActivity {
         etCpf = findViewById(R.id.etCpf);
         etEmail = findViewById(R.id.etEmail);
         etSenha = findViewById(R.id.etSenha);
-        progressBar = findViewById(R.id.progressBar); // Inicializa a ProgressBar
+        progressBar = findViewById(R.id.progressBar);
 
         Button btnRegistrar = findViewById(R.id.btnRegistrar);
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registrar(v);
-            }
-        });
+        btnRegistrar.setOnClickListener(this::registrar);
     }
 
     private void registrar(View v) {
-        String nome = etNome.getText().toString();
-        String cpf = etCpf.getText().toString();
-        String email = etEmail.getText().toString();
-        String senha = etSenha.getText().toString();
+        String nome = etNome.getText().toString().trim();
+        String cpf = etCpf.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String senha = etSenha.getText().toString().trim();
 
-        if(nome.isEmpty()){
-            etNome.setError("Campo obrigatório");
+        if (nome.isEmpty()) {
+            etNome.setError("Nome obrigatório");
             etNome.requestFocus();
             return;
         }
-        if(cpf.isEmpty()){
-            etCpf.setError("Campo obrigatório");
+
+        if (cpf.isEmpty()) {
+            etCpf.setError("CPF obrigatório");
             etCpf.requestFocus();
             return;
         }
 
-        if(email.isEmpty()){
-            etEmail.setError("Campo obrigatório");
+        if (!validarCpf(cpf)) {
+            etCpf.setError("CPF inválido");
+            etCpf.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            etEmail.setError("E-mail obrigatório");
             etEmail.requestFocus();
             return;
         }
-        if(senha.isEmpty()){
-            etSenha.setError("Campo obrigatório");
+
+        if (senha.isEmpty()) {
+            etSenha.setError("Senha obrigatório");
             etSenha.requestFocus();
             return;
         }
 
-        // Exibe a ProgressBar antes de iniciar o registro
         progressBar.setVisibility(View.VISIBLE);
 
-
-
-        // Consulta ao Firestore para verificar se o CPF já existe
         Query query = db.collection("usuarios").whereEqualTo("cpf", cpf);
         query.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 QuerySnapshot querySnapshot = task.getResult();
                 if (querySnapshot != null && !querySnapshot.isEmpty()) {
-                    // CPF já existe
                     Snackbar.make(v, "Erro ao cadastrar, usuário existente", Snackbar.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE); // Oculta a ProgressBar
+                    progressBar.setVisibility(View.GONE);
                 } else {
-                    // CPF não existe, prossiga com o registro
                     mAuth.createUserWithEmailAndPassword(email, senha)
                             .addOnCompleteListener(RegistroActivity.this, task1 -> {
                                 if (task1.isSuccessful()) {
                                     String uid = mAuth.getUid();
-                                    // Registro bem-sucedido, adicione os dados ao Firestore
                                     Registro registro = new Registro(uid, nome, cpf, email);
                                     db.collection("usuarios").document(uid).set(registro)
                                             .addOnSuccessListener(aVoid -> {
@@ -111,22 +107,30 @@ public class RegistroActivity extends AppCompatActivity {
                                             })
                                             .addOnFailureListener(e -> {
                                                 Snackbar.make(v, "Erro ao registrar dados no Firestore", Snackbar.LENGTH_SHORT).show();
-                                                progressBar.setVisibility(View.GONE); // Oculta a ProgressBar
+                                                progressBar.setVisibility(View.GONE);
                                             });
                                 } else {
-                                    // Erro ao criar usuário, exiba uma mensagem de erro
                                     Snackbar.make(v, "Erro ao criar usuário", Snackbar.LENGTH_SHORT).show();
-                                    progressBar.setVisibility(View.GONE); // Oculta a ProgressBar
+                                    progressBar.setVisibility(View.GONE);
                                 }
                             });
                 }
             } else {
                 Log.e("FirestoreError", "Erro ao acessar o Firestore: " + task.getException().getMessage());
-                Snackbar.make(v, "Erro ao acessar o Firestore: " + task.getException().getMessage(), Snackbar.LENGTH_SHORT).show();
-                progressBar.setVisibility(View.GONE); // Oculta a ProgressBar
+                Snackbar.make(v, "Erro ao acessar o Firestore", Snackbar.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
             }
         });
     }
 
-
+    private boolean validarCpf(String cpf) {
+        CPFValidator cpfValidator = new CPFValidator();
+        try {
+            cpfValidator.assertValid(cpf);
+            return true;
+        } catch (InvalidStateException e) {
+            Log.e("CPFValidation", "Erro ao validar CPF: " + e.getInvalidMessages());
+            return false;
+        }
+    }
 }
