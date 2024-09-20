352294183207fb3b3bbc8d8a2b86bfbee649e825
leonardo.goandete <leonardo.goandete@hotmail.com>
2024-09-20 13:17:34 -0300

    Add host IFRS

--- app/src/main/java/br/com/ifrs/meuifpoa/utils/Constants.java
+++ app/src/main/java/br/com/ifrs/meuifpoa/utils/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
     //private static final String BASE_URL = "http://192.168.0.23:8080/";
     //private static final String BASE_URL = "https://app-ifrs-back-5ee5bb142a80.herokuapp.com/";
     //private static final String BASE_URL = "https://2d15-200-180-191-23.ngrok-free.app/";
-    public static final String BASE_URL = "http://leogoandete.tplinkdns.com:8080/";
+    //public static final String BASE_URL = "http://leogoandete.tplinkdns.com:8080/";
+    public static final String BASE_URL = "http://app.poa.ifrs.edu.br:8080/";
     public static final String BASE_URL_NOTICIA = "https://poa.ifrs.edu.br";
     public static final String DOC_HISTORICO = "historico";
     public static final String DOC_HISTORICO_EMENTAS = "historicoEmentas";
